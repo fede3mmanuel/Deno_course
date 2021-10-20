88243bac444efb59a8732e291046c13cbcbfc8c9
fede3mmanuel <fede@fede.website>
2021-10-20 16:42:17 -0500

    get product response works

--- controllers/products.ts
+++ controllers/products.ts
@@ -36,8 +36,21 @@ const getProducts = ({ response }: {response: any}) => {
 
 // @route Get /api/v1/products/:id
 
-const getProduct = ({ response }: {response: any}) => {
-    response.body = 'get'   
+const getProduct = ({ params, response }: { params: { id: string }, response: any}) => {
+    const product: Product | undefined = products.find(p => p.id === params.id)
+    if (product) {
+        response.status = 200
+        response.body = {
+            success: true,
+            data: product
+        }
+    } else {
+        response.status = 404
+        response.body = {
+            success: false,
+            msg: 'No product found'
+        }
+    }
 }
 
 // @desc Add a product
